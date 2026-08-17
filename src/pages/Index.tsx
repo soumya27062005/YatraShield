af0841df60e68b4638790352f995c422cb5c1a49
Soumya Gupta <166936873+soumya27062005@users.noreply.github.com>
2026-08-17 22:17:23 +0530

    Create Index.tsx with AppSelector component

---
+++ src/pages/Index.tsx
@@ -0,0 +1,9 @@
+// Update this page (the content is just a fallback if you fail to update the page)
+
+import AppSelector from '../components/AppSelector';
+
+const Index = () => {
+  return <AppSelector />;
+};
+
+export default Index;
